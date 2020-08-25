e6dacf2d9b7e8e4a76106e3fd17b8e09b67c82a1
mdogucu <mdogucu@gmail.com>
2020-08-25 03:22:13 -0700

    add week 4

--- schedule/R/quiz-3-grid-app.R
+++ schedule/R/quiz-3-grid-app.R
@@ -26,25 +26,3 @@ post_sample <- sample_n(___, size = ___,
                            weight = ___, replace = ___)
 
 
-# Step 1: Define a grid of 501 lambda values
-lambda_grid <- seq(from = 0, to = 15, length = 501)
-grid_data <- data.frame(lambda_grid)
-# Step 2: Evaluate the prior & likelihood at each lambda
-grid_data <- grid_data %>%
-  mutate(prior = dgamma(lambda_grid, 3, 1)) %>%
-  mutate(likelihood = dpois(5, lambda_grid))
-# Step 3: Approximate the posterior
-grid_data <- grid_data %>%
-  mutate(unnormalized = likelihood * prior) %>%
-  mutate(posterior = unnormalized / sum(unnormalized))
-# Set the seed
-set.seed(84735)
-# Step 4: sample from the discretized posterior
-post_sample <- sample_n(grid_data, size = 10000,
-                        weight = posterior, replace = TRUE)
-
-# Histogram of the grid simulation with posterior pdf
-ggplot(post_sample, aes(x = lambda_grid)) +
-  geom_histogram(aes(y = ..density..), color = "white") +
-  stat_function(fun = dgamma, args = list(8, 2)) +
-  lims(x = c(0, 15))
